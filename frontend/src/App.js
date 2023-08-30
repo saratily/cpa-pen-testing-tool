d5cc0e8b768561107186229ecbe98e4549b45582
Sara Tily <saratily@gmail.com>
2023-08-30 16:01:17 -0400

    Add frontend code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import PostsPage from './pages/PostsPage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
+
 function App() {
+/*
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +30,46 @@ function App() {
       </header>
     </div>
   );
+  */
+
+          /*
+        <Route path="/posts">
+          {authContext.loggedIn && <PostsPage />}
+          {!authContext.loggedIn && <Navigate to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Navigate to="/"/>
+        </Route>
+
+
+        <Route path="/posts">
+          {authContext.loggedIn && navigate("/posts")}
+          {!authContext.loggedIn && navigate("/auth")}
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+                
+        <Route path="*" element={<HomePage />} />
+
+
+        */
+
+  const authContext = useContext(AuthContext);
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" exact element={<HomePage/>} />
+        {!authContext.loggedIn && (
+          <Route path="/auth" element={<AuthPage />} />
+        )}
+        <Route path="/posts" element=
+          {authContext.loggedIn ? <PostsPage /> : <AuthPage /> } />      
+
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Layout>
+  );
+
 }
 
 export default App;
