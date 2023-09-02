3bc823e37e04f54001d44fd07297b522f788e32a
Sara Tily <saratily@gmail.com>
2023-09-02 03:19:32 -0400

    Tabs working

--- frontend/src/components/DNSEnumeration/DNSEnumeration.js
+++ frontend/src/components/DNSEnumeration/DNSEnumeration.js
@@ -1,5 +1,13 @@
-function DNSEnumeration() {
-    return <div className='page'>DNSEnumeration</div>
-}
+import './styles.css';
+import Tabs from './TabComponent/Tabs';
 
+function DNSEnumeration() {
+ return (
+       <div>
+         <h1>DNS Enumeration</h1>
+         <Tabs/>
+         <footer><a href=''>react-simple-tabs</a></footer>
+       </div>
+     );
+ }
 export default DNSEnumeration;
